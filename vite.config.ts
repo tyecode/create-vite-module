05b5da40fca434a932bb82dd937226179d3c9eb0
tyecode <sengphachanh.dev@gmail.com>
2024-01-22 10:24:23 +0700

    Add Vite configuration and dependencies

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/components/index.tsx"),
+      name: "tyecode/create-vite-module",
+      fileName: "create-vite-module",
+      formats: ["umd"],
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
